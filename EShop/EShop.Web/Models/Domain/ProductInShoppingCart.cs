923c18bc352ba9ce7dfa06b444b2ead991c79287
Your Name <you@example.com>
2023-04-09 18:51:10 +0200

    Add project files.

---
+++ EShop/EShop.Web/Models/Domain/ProductInShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Web.Models.Domain
+{
+    public class ProductInShoppingCart
+    {
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public Guid ShoppingCartId { get; set; }
+
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
